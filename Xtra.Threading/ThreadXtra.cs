922030ca81c082543342d60fc19fa05194722787
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-04 11:58:31 +0600

    Basic Implementation is done

---
+++ Xtra.Threading/ThreadXtra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Xtra.Threading
+{
+  public static class ThreadXtra
+  {
+    public static void RunInNewThread(Action action)
+    {
+      var thread = new Thread(() => action());
+      thread.Start();
+    }
+  }
+}
