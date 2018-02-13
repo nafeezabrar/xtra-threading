c2a8493ce1d0f7b5754aebdf847894c8247ff1f8
Nafeez Abrar <nafeezabrar@gmail.com>
2018-02-13 14:30:16 +0600

    Underline thread object is returned

--- Xtra.Threading/ThreadXtra.cs
+++ Xtra.Threading/ThreadXtra.cs
@@ -4,28 +4,32 @@ namespace Xtra.Threading
 {
   public static class ThreadXtra
   {
-    public static void RunInNewThread(ParameterizedThreadStart start)
+    public static Thread RunInNewThread(ParameterizedThreadStart start)
     {
       var thread = new Thread(start);
       thread.Start();
+      return thread;
     }
 
-    public static void RunInNewThread(ParameterizedThreadStart start, int maxStackSize)
+    public static Thread RunInNewThread(ParameterizedThreadStart start, int maxStackSize)
     {
       var thread = new Thread(start, maxStackSize);
       thread.Start();
+      return thread;
     }
 
-    public static void RunInNewThread(ThreadStart start)
+    public static Thread RunInNewThread(ThreadStart start)
     {
       var thread = new Thread(start);
       thread.Start();
+      return thread;
     }
 
-    public static void RunInNewThread(ThreadStart start, int maxStackSize)
+    public static Thread RunInNewThread(ThreadStart start, int maxStackSize)
     {
       var thread = new Thread(start, maxStackSize);
       thread.Start();
+      return thread;
     }
   }
 }
